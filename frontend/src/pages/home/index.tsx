2dc2d38b1292caefa43a779b911c74c1aecc1835
2022-DEV3-107 <ahmetunaz@hotmail.com>
2022-12-09 17:06:09 +0100

    Tailwind Styled Components is implemented

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Container } from "./style";
 
 export interface Props {}
 
 const Home = (props: Props) => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Container>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Container>
+  );
 };
 
 export default Home;
--- frontend/src/pages/home/style.ts
+++ frontend/src/pages/home/style.ts
@@ -1,1 +1,13 @@
-export {};
+import tw from "tailwind-styled-components";
+
+const Container = tw.div`
+    flex
+    items-center
+    justify-center
+    flex-col
+    w-full
+    min-h-screen
+    bg-indigo-600
+`;
+
+export { Container };
