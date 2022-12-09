4977ff5864786e50eec8814da4926450dc877370
2022-DEV3-107 <ahmetunaz@hotmail.com>
2022-12-09 16:56:24 +0100

    Tailwindcss is integrated

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export interface Props {}
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
