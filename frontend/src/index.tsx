de6d9551de402fa3bdac002c3a285bc1cebf3333
2022-DEV3-107 <ahmetunaz@hotmail.com>
2022-12-09 16:37:18 +0100

    React Router Dom is implemented

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
@@ -18,7 +19,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
---
+++ frontend/src/pages/not-found/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface Props {}
+
+const NotFound = (props: Props) => {
+  return <div>NotFound</div>;
+};
+
+export default NotFound;
